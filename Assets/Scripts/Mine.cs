a7fcc777ad02cbaa5fb3b721b1e1abcd5c93bd3d
TroianArt <troian.art@gmail.com>
2023-05-28 15:10:38 +0300

    Mine

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : Entity
+{
+    [SerializeField] private int damage;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.TryGetComponent<Entity>(out var entity))
+        {
+            entity.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
